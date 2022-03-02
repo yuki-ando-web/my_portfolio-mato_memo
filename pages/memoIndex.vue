9be0fb89a1dc5f6d5db3e5e530c7de886362a81b
yuki-ando-web <yukiwork1998@gmail.com>
2022-03-02 21:56:26 +0900

    add tag parts

--- pages/memoIndex.vue
+++ pages/memoIndex.vue
@@ -4,45 +4,89 @@
       <v-content>
         <v-container class="grey lighten-5 pr-0">
           <v-responsive>
-            <v-row p-0>
+            <v-row>
+              <v-navigation-drawer>
+                <v-card height="400" width="256">
+                  <v-list-item>
+                     </v-list-item>
+                </v-card>
+              </v-navigation-drawer>
               <div>
-
-              <v-col cols>
-                <div v-for="(memo, index) in memos" v-bind:key="index">
-                  <v-card height="144" width="144">
-                    <v-card-text>
-                      <p class="text-caption m-0 p-0">{memo.title}</p>
-                      <p class="text-caption">
-                        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-                      </p>
-                    </v-card-text>
-                  </v-card>
-                </div>
-              </v-col>
+                <v-col cols>
+                  <div v-for="(memo, index) in memos" v-bind:key="index">
+                    <v-card height="144" width="144">
+                      <v-card-text>
+                        <div class="text-caption mt-n3 ml-n3 p-0">
+                          {{ memo.title }}
+                        </div>
+                        <div class="text-caption mt-n1 ml-n2 mb-n2">
+                          兄貴とアノニマスとアニサキスとアナウンスふぁさfsっっfsふぇあっふぇげrgrgsっgらあっgvgrg
+                        </div>
+                      </v-card-text>
+                    </v-card>
+                  </div>
+                </v-col>
               </div>
+
               <v-col cols="">
                 <v-card>
-                  <v-textarea auto-grow dense min-height></v-textarea>
+                  <div>
+                    <v-textarea auto-grow dense height="100%"></v-textarea>
+                  </div>
                   <v-divider class="my-2"></v-divider>
 
                   <v-row>
                     <div class>
                       <v-col class="mr-n4 text-no-wrap text-caption">
-                        <p class="">タグ</p>
+                        タグ
                       </v-col>
                     </div>
-                    <div class height=3px>
-                      <v-col class="mt-n6 pt-n6" >
-                        <v-text-field text-caption outlined ></v-text-field>
-                      </v-col>
-                    </div>
-                    <div>
-                      <v-col>
-                        <p class="text-caption">タグ削除</p>
+
+                    <div height="3px">
+                      <v-col class="mt-n6 pt-n6 text-caption">
+                        <v-card-actions>
+                          <v-text-field
+                            text-caption
+                            placeholder="タグを入力"
+                          ></v-text-field>
+                        </v-card-actions>
                       </v-col>
                     </div>
                   </v-row>
+                  <v-container>
+                    <v-row no-gutters align-content="">
+                      <v-col
+                        v-for="(memo, index) in memos"
+                        v-bind:key="index"
+                        style="position: sticky; left: 0"
+                      >
+                        <v-chip close filter ripple tag small></v-chip>
+                      </v-col>
+                    </v-row>
+                  </v-container>
                 </v-card>
+                <!-- <div style="overflow-x: auto;"> -->
+                <div style="overflow-x: auto">
+                  <v-virtul-scroll
+                    max-height="20"
+                    item-height="20"
+                    items="memos"
+                  >
+                    <v-card>
+                      <v-list>
+                        <v-list
+                          v-for="(memo, index) in memos"
+                          v-bind:key="index"
+                          style="position: sticky; left: 0"
+                        >
+                          {{ memo.title }}
+                        </v-list>
+                      </v-list>
+
+                      <!-- <v-chip close filter ripple tag small> 取引先あああ</v-chip> -->
+                    </v-card>
+                  </v-virtul-scroll>
+                </div>
               </v-col>
             </v-row>
           </v-responsive>
@@ -58,6 +102,7 @@ export default {
     return {
       memos: [
         { title: 'aaaaa', content: 'bbbbbb' },
+        { title: 'あああ', content: 'sddddd' },
         { title: 'cccc', content: 'sddddd' },
         { title: 'cccc', content: 'sddddd' },
         { title: 'cccc', content: 'sddddd' },
@@ -69,6 +114,9 @@ export default {
         { title: 'cccc', content: 'sddddd' },
       ],
     }
+  },
+  created() {
+    console.log(this.memos)
   },
 }
 </script>
