0b273212093e283edc3d5384b7ef6e31e56ebc24
yuki-ando-web <yukiwork1998@gmail.com>
2022-03-14 15:59:56 +0900

    firebaseを追加

---
+++ plugins/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: 'AIzaSyCmipcCawSXUjxeZXTpf1bX1AJRjsuzoUQ',
+    authDomain: 'tag-memo-app.firebaseapp.com',
+    projectId: 'tag-memo-app',
+    storageBucket: 'tag-memo-app.appspot.com',
+    messagingSenderId: '892895605720',
+    appId: '1:892895605720:web:68146386b4d47109ab38ad',
+    measurementId: 'G-SRX1BQMK7P',
+  })
+}
+
+export default firebase
